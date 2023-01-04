dde7337fcba4e21a01f62bf03ef75d653a12a328
Edilson Rodrigues <edilson.w3g@gmail.com>
2023-01-04 19:59:41 -0300

    chore: study

---
+++ src/go-programming-language/chapter4/autoescape/autoescape.go
@@ -0,0 +1,23 @@
+// Suppressing auto-escaping behavior for fields that contain trusted HTML data
+// Using the named string type template.HTML instead of string
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+func main() {
+	const templ = `<p>A: {{.A}}</p><p>B: {{.B}}</p>`
+	t := template.Must(template.New("escape").Parse(templ))
+	var data struct {
+		A string        // untrusted plain HTML
+		B template.HTML // trusted HTML
+	}
+	data.A = "<b>Hello!</b>"
+	data.B = "<b>Hello!</b>"
+	if err := t.Execute(os.Stdout, data); err != nil {
+		log.Fatal(err)
+	}
+}
